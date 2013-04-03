e9f54e255533dd13c24df573ba34ac0c01694520
Vladimir Agafonkin <agafonkin@gmail.com>
2013-04-03 19:30:33 +0300

    make DomUtil.create className optional

--- src/dom/DomUtil.js
+++ src/dom/DomUtil.js
@@ -92,7 +92,10 @@ L.DomUtil = {
 	create: function (tagName, className, container) {
 
 		var el = document.createElement(tagName);
-		el.className = className;
+
+		if (className) {
+			el.className = className;
+		}
 
 		if (container) {
 			container.appendChild(el);
